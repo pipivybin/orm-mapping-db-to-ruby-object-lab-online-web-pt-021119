36b4edd0541832adaee1a8d4f6ab7299b1dbf16f
Ming Duan <11203duan@gmail.com>
2019-04-30 23:47:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,8 +20,7 @@ attr_accessor :id, :name, :grade
       |row| self.new_from_db(row)
     end
     @@all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    
   end
 
   def self.find_by_name(name)
