1161184e255e2bfaf88003d3342131e3cfb1abd6
Ming Duan <11203duan@gmail.com>
2019-05-01 00:04:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,8 +35,8 @@ def self.students_below_12th_grade
   DB[:conn].execute("SELECT id, name, grade FROM students WHERE grade < ?", 12)
 end
 
-def self.first_X_students_in_grade_10
-
+def self.first_X_students_in_grade_10(x)
+  DB[:conn].execute("SELECT id, name, grade FROM students WHERE grade = ?", 9)
 end
 
 def self.all_students_in_grade_X
