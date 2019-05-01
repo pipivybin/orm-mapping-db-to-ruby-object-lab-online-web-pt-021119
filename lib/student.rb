915943ffeef0a203c30c29965da6aea46aab2e8f
Ming Duan <11203duan@gmail.com>
2019-05-01 00:12:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,66 @@
+require 'pry'
+
 class Student
-  attr_accessor :id, :name, :grade
+
+attr_accessor :id, :name, :grade
+@@all = []
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+
+    @student = self.new
+    @student.id = row[0]
+    @student.name = row[1]
+    @student.grade = row[2]
+    @student
+
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    arry = DB[:conn].execute("SELECT * FROM students")
+    arry.collect do
+      |row| self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    arry = DB[:conn].execute("SELECT id, name, grade FROM students WHERE name = ? LIMIT 1", name).first
+    self.new_from_db(arry)
   end
-  
+
+def self.all_students_in_grade_9
+  DB[:conn].execute("SELECT id, name, grade FROM students WHERE grade = ?", 9)
+end
+
+def self.students_below_12th_grade
+  arry = DB[:conn].execute("SELECT id, name, grade FROM students WHERE grade < ?", 12)
+  arry.collect do
+    |row| self.new_from_db(row)
+  end
+
+end
+
+def self.first_X_students_in_grade_10(x)
+  DB[:conn].execute("SELECT id, name, grade FROM students WHERE grade = ? LIMIT ?", 10, x)
+end
+
+def self.first_student_in_grade_10
+  arry = DB[:conn].execute("SELECT id, name, grade FROM students WHERE grade = 10 LIMIT 1").first
+  self.new_from_db(arry)
+end
+
+def self.all_students_in_grade_X(x)
+  DB[:conn].execute("SELECT id, name, grade FROM students WHERE grade = ?", x)
+end
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
