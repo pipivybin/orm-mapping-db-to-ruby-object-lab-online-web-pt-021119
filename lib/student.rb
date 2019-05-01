892b826e862b2e8467bd76abc95d9dc4e9213c67
Ming Duan <11203duan@gmail.com>
2019-05-01 00:02:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,19 +28,19 @@ attr_accessor :id, :name, :grade
   end
 
 def self.all_students_in_grade_9
-  
+
 end
 
 def self.students_below_12th_grade
-  
+
 end
 
 def self.first_X_students_in_grade_10
-  
+
 end
 
 def self.all_students_in_grade_X
-  
+
 end
 
   def save
